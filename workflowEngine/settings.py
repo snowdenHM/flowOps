9f1ad69260752779f91f7e14be8b82be2164e76c
Atishay Gwari <83054615+atishay-gwari@users.noreply.github.com>
2023-05-22 12:28:51 +0530

    Update settings.py
    Removed mail details

--- workflowEngine/settings.py
+++ workflowEngine/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'atishay345@gmail.com'
-EMAIL_HOST_PASSWORD = 'nlykikrsmcjtgonn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
